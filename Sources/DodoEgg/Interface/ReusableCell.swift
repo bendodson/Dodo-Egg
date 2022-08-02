a76ded23ee1d926a6cf3760213a96ff651c7b857
Ben Dodson <ben@bendodson.com>
2022-08-02 17:44:12 +0100

    Reworking ReusableCell generic so identifier still contains the generic

--- Sources/DodoEgg/Interface/ReusableCell.swift
+++ Sources/DodoEgg/Interface/ReusableCell.swift
@@ -10,8 +10,8 @@ extension ReusableCell {
     
     public static func register(_ tableView: UITableView) {
         // Separating by < so we can catch generic implementations i.e. CardTableViewCell<Treasure>
-        let identifier = String(describing: self).components(separatedBy: "<")[0]
-        tableView.register(UINib(nibName: identifier, bundle: nil), forCellReuseIdentifier: identifier)
+        let identifier = String(describing: self)
+        tableView.register(UINib(nibName: identifier.components(separatedBy: "<")[0], bundle: nil), forCellReuseIdentifier: identifier)
     }
     
     public static func dequeue(_ tableView: UITableView, indexPath: IndexPath) -> Self {
