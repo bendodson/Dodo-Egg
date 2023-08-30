2ffa871b882c0a8eb31ef93792e05edc376489e7
Ben Dodson <ben@bendodson.com>
2023-08-30 16:57:02 +0100

    Setting iOS 12 as the new minimum

--- Sources/DodoEgg/Interface/View-IfModifier.swift
+++ Sources/DodoEgg/Interface/View-IfModifier.swift
@@ -1,7 +1,9 @@
 // Developed by Ben Dodson (ben@bendodson.com)
 
+
 import SwiftUI
 
+@available(iOS 13.0, *)
 extension View {
     /// Applies the given transform if the given condition evaluates to `true`.
     /// - Parameters:
